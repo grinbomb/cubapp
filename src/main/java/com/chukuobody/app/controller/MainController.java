b9a8feeade2613a09a76b4ee742a482dc6c7b8e8
NGM <grinbomb@mail.ru>
2020-11-26 17:08:26 +0300

    Create the cub-app with fremarker,DB,Spring

---
+++ src/main/java/com/chukuobody/app/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.chukuobody.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	@GetMapping("/main")
+	public String main(@RequestParam(required = false, defaultValue = "") String name, Model model) {
+		model.addAttribute("name", name);
+        return "main";
+    }
+}
